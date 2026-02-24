5353f9ef2caaec226a1795c1ddfc645a3263805d
TBWLJ <taiwoayomide202@gmail.com>
2026-02-24 09:27:23 +0100

    change url

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://kwolity-backend.onrender.com/api",
+  baseURL: "https://api.kwolitygroupltd.com.ng/api",
   withCredentials: true,
 });
 
